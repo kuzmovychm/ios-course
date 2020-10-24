36485a654a8873da16f219f1ba833ab5e4ae70a2
Max Kuzmovych <max.kuzmovych@botscrew.com>
2020-10-24 23:30:10 +0300

    generate .gitignore file, generate base project in Xcode

---
+++ mobile-lab/mobile-lab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mobile-lab
+//
+//  Created by Max on 24.10.2020.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
