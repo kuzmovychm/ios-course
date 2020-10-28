76caa82ce572ebddc6cd2f5c83e45a2a61fb39ea
Max Kuzmovych <max.kuzmovych@botscrew.com>
2020-10-28 12:40:21 +0200

    update print()

--- mobile-lab/mobile-lab/ContentView.swift
+++ mobile-lab/mobile-lab/ContentView.swift
@@ -8,14 +8,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View {    
     var body: some View {
-        Text("Hello, World!")
+        print("Hello World")
+        return Text("Hello, World!")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        return ContentView()
     }
 }
